19c8b40b9fd6b808910c3a937e4a978610471b1a
Josip Vulic <jovulic@gmail.com>
2025-04-10 22:27:37 -0400

    ftr(ctl): update bootstrap to take a yes flag

--- ctl/src/bootstrap_command.sh
+++ ctl/src/bootstrap_command.sh
@@ -1,6 +1,12 @@
 # shellcheck shell=bash
 
 device_path="${args[device]}"
+yes="${args[--yes]}"
+
+if [ -n "$yes" ]; then
+  # Refresh sudo credentials if necessary.
+  sudo -v
+fi
 
 store_path=$(nix build .#bootstrap --print-out-paths)
 iso_path_files=("$store_path/iso/nixos-"*.iso)
@@ -10,11 +16,20 @@ if [ ! -f "$iso_path" ]; then
   exit 1
 fi
 
-echo "Do you want to write $iso_path to $device_path? (y/N)"
-read -r answer
-if [[ "$answer" =~ ^[Yy]$ ]]; then
+unset write
+if [ -n "$yes" ]; then
+  write=1
+else
+  echo "Do you want to write $iso_path to $device_path? (y/N)"
+  read -r answer
+  if [[ "$answer" =~ ^[Yy]$ ]]; then
+    write=1
+  fi
+fi
+
+if [ -n "$write" ]; then
   echo "writing to device..."
   sudo dd bs=4M if="$iso_path" of="$device_path" status=progress conv=fsync
 else
-  echo "not performing write"
+  echo "cancelled"
 fi
