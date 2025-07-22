fb1b2f7fb8a937ed4264bfefecc243832b6cba23
Josip Vulic <jovulic@gmail.com>
2025-07-21 21:36:44 -0400

    ftr(tools): add tools derivation

--- modules/default.nix
+++ modules/default.nix
@@ -2,13 +2,14 @@
 {
   imports = [
     ./firefox.nix
-    ./remote-desktop.nix
     ./google-chrome.nix
     ./hardware.nix
     ./neovim.nix
     ./nix.nix
     ./nvidia.nix
+    ./remote-desktop.nix
     ./software.nix
     ./steam.nix
+    ./tools.nix
   ];
 }
---
+++ modules/tools.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.outpost.tools;
+in
+with lib;
+{
+  options = {
+    outpost.tools = {
+      enable = mkOption {
+        type = types.bool;
+        description = "Enable tools configuration.";
+        default = true;
+      };
+    };
+  };
+  config = mkIf cfg.enable {
+    environment.systemPackages = [
+      pkgs.zip # compressor/archiver for creating and modifying zipfiles
+      pkgs.unzip # extraction utility for archives compressed in .zip format
+    ];
+  };
+}
